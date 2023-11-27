6ea86880ded4d1b7f381c2685368b22bda1b9c26
Home <black@galaxy>
2023-11-27 11:04:45 +0100

    created TopNavigationBar.tsx and folder structure

---
+++ ust_dig/src/components/top_navigation_bar/TopNavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TopNavigationBar() {
+    return (
+        <nav>
+            <ul>
+                <li>Logo</li>
+                <li>Mixes</li>
+                <li>Visuals</li>
+                <li>Merch</li>
+                <li>About</li>
+            </ul>
+        </nav> 
+        );
+};
