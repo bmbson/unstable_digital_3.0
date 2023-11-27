4800893e72418a230ca113b0539c212e8ed8f1e6
db <d.blackson@hva.nl>
2023-11-27 15:35:06 +0100

    work on cables patches

--- ust_dig/src/app/visuals/page.tsx
+++ ust_dig/src/app/visuals/page.tsx
@@ -1,9 +1,12 @@
+import CablesPatch from "@/components/cable_patch/cables_patch"
 import React from "react"
 
 export default function Visuals() {
     return (
       <main>
         <p>Visuals</p>
+        <CablesPatch/>
+        <CablesPatch patchDir={"/patch1/"} canvasId={"othercanvas"} patchOptions={{ "glCanvasResizeToWindow": false }}/>
       </main>
     )
   }
