9614ff92e7890859b0fbb5381191bf1445b1ab19
db <d.blackson@hva.nl>
2023-11-30 11:16:05 +0100

    work on mobile/desktop reponsiveness

--- ust_dig/src/components/top_navigation_bar/TopNavigationBar.tsx
+++ ust_dig/src/components/top_navigation_bar/TopNavigationBar.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 import './TopNavigationBar.css'
+
+const hamburger_menu_style = {
+    color: 'white',
+    border: '1px solid #fff',
+    cursor: 'pointer',
+  }
 
 function TopNavigationBar() {
     return (
         <nav>
             <ul id="TopNavigationBar">
-                <div id="LeftSideTopNavigationBar">
+                <Image id="SmallScreenNavigationMenu" src="/top_navigation_bar_images/hamburger_menu.svg" alt={"Menu"} width={50} height={50} style={hamburger_menu_style}/>
+                <div id="Logo">
                     <li><Link href="/">Logo</Link></li>
                 </div>
-                <div id="RightSideTopNavigationBar">
+                
+                <div id="BigScreenNavigationMenu">
                     <li><Link href="/mixes">Mixes</Link></li>
                     <li><Link href="/visuals">Visuals</Link></li>
                     <li><Link href="/store">Store</Link></li>
                     <li><Link href="/account">Account</Link></li>                
                     <li><Link href="/about">About</Link></li>
                 </div>
+
             </ul>
         </nav>
     );
