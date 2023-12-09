b18a73f32eb01adfaf442bac73786ab89ea07168
Home <black@galaxy>
2023-12-09 22:27:59 +0100

    add hamburger menu image

--- ust_dig/src/components/top_navigation_bar/TopNavigationBar.tsx
+++ ust_dig/src/components/top_navigation_bar/TopNavigationBar.tsx
@@ -4,27 +4,26 @@ import Link from "next/link";
 import './TopNavigationBar.css'
 
 const hamburger_menu_style = {
-    color: 'white',
-    border: '1px solid #fff',
     cursor: 'pointer',
-  }
+    filter: 'brightness(0) invert(1)',
+}
 
 function TopNavigationBar() {
     return (
         <nav>
             <ul id="TopNavigationBar">
-                <Image className="SmallScreenNavigationMenu" src="/top_navigation_bar_images/hamburger_menu.svg" alt={"Menu"} width={50} height={50} style={hamburger_menu_style}/>
+                <Image className="SmallScreenNavigationMenu" src="/images/top_navigation_bar_images/hamburger_menu.svg" alt={"Menu"} width={50} height={50} style={hamburger_menu_style} />
                 <div id="Logo">
                     <li><Link href="/">Logo</Link></li>
                 </div>
 
-                <p className="SmallScreenNavigationMenu">Login</p>
-                
+                <Link href="/account" className="SmallScreenNavigationMenu">Account</Link>
+
                 <div id="BigScreenNavigationMenu">
                     <li><Link href="/mixes">Mixes</Link></li>
                     <li><Link href="/visuals">Visuals</Link></li>
                     <li><Link href="/store">Store</Link></li>
-                    <li><Link href="/account">Account</Link></li>                
+                    <li><Link href="/account">Account</Link></li>
                     <li><Link href="/about">About</Link></li>
                 </div>
 
