0ea547975db5973691e60da30fc375c6dbd7b044
db <d.blackson@hva.nl>
2023-12-24 01:12:46 +0100

    add toggle on play pause icon

--- ust_dig/src/components/bottom_audio_control_bar/BottomAudioControlBar.tsx
+++ ust_dig/src/components/bottom_audio_control_bar/BottomAudioControlBar.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import './BottomAudioControlBar.css';
 import { MdPlayArrow, MdPause } from "react-icons/md";
 
 function BottomAudioControlBar() {
+
+    const [playPauseState, setPlayPauseState] = useState(false);
+
+    function playPauseControl() {
+        setPlayPauseState(!playPauseState)
+    }
+
+    function volumeBarControl() {
+        alert();
+    }
+
+    function seekerBarControl() {
+        alert();
+    }
+
     return (
         <div id="BottomAudioControlBar">
             <audio controls>
-                <source src="/nationHoe.wav" type="audio/wav"/>
+                <source src="/nationHoe.wav" type="audio/wav" />
             </audio>
-            <div id="playPause">Play/Pause</div>
+            <button onClick={() => playPauseControl()} id="playPauseButton">
+                {playPauseState ? <MdPlayArrow></MdPlayArrow> : <MdPause></MdPause>}
+            </button>
             <input id="VolumeControl" type="range" />
-            <input id="SeekerBar "type="range" />
+            <input id="SeekerBar " type="range" />
             <div id="AudioInfo">
+                <img src="" alt="" />
                 <p>MixName</p>
                 <p>ArtistName</p>
             </div>
