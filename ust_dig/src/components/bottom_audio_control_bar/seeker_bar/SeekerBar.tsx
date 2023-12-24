1d0c84e6c6304ebaa00e3172fdfa7f33e02b20dd
db <d.blackson@hva.nl>
2023-12-24 13:13:07 +0100

    create/think about seekerbar functionality

--- ust_dig/src/components/bottom_audio_control_bar/seeker_bar/SeekerBar.tsx
+++ ust_dig/src/components/bottom_audio_control_bar/seeker_bar/SeekerBar.tsx
@@ -1,6 +1,18 @@
 import './SeekerBar.css';
 
 function SeekerBar() {
+    function minutesSecondsNotationToSeconds(minutesSecondsTime: String): undefined {
+        return undefined;
+    }
+
+    function secondsNotationToMinutesSeconds(secondsTime: String): undefined {
+        return undefined;
+    }
+
+    function calculateCurrentTime(): undefined {
+        return undefined;
+    }
+
     return <div id="seekerBar">
         <p id="currentAudioTime">0:00</p>
         <input id="seekerBarSlider" type="range" />
