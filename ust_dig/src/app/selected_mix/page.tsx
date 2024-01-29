702e586b8b6fbf73c2567d0021f0f54a929b96a3
black@system.com <black@system.com>
2024-01-29 16:39:56 +0100

    finish basic mixcards with images

--- ust_dig/src/app/selected_mix/page.tsx
+++ ust_dig/src/app/selected_mix/page.tsx
@@ -4,7 +4,7 @@ import './page.css';
 export default function Selected_Mix() {
     return (
         <main>
-
+            <div>test</div>
 
         </main>
     )
--- ust_dig/src/components/mix_card/mix_card.tsx
+++ ust_dig/src/components/mix_card/mix_card.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import './mix_card.css'
+import Link from 'next/link';
 
 interface props {
     'mixName': string;
@@ -11,19 +12,23 @@ interface props {
 function MixCard(props: props) {
     return (
         <>
-            <div className="mixCard">
-                <div className="image">imageSrc</div>
-                <div className="bottomInfo">
-                    <div className="bottomInfoLeft">
-                        <p>{props.mixName}</p>
-                        <p>{props.artist}</p>
-                    </div>
-                    <div className="bottomInfoRight">
-                        <p>{props.date}</p>
+            <Link href={'/selected_mix'}>
+                <div className="mixCard">
+                    <div className="image" style={{ position: 'relative' }}>
+                        <Image fill={true} layout='fill' src={'/images/testImage.jpg'} alt={'Test Image'}>
+                        </Image>
+                        <div className="bottomInfo">
+                            <div className="bottomInfoLeft">
+                                <p>{props.mixName}</p>
+                                <p>{props.artist}</p>
+                            </div>
+                            <div className="bottomInfoRight">
+                                <p>{props.date}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-            </div>
+            </Link>
         </>
     )
 };
