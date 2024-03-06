6d53435c7efd3090dea252d5feba2bb6129656e6
black@system.com <black@system.com>
2024-03-06 12:28:36 +0100

    add artist info image and mixpanel

--- ust_dig/src/app/selected_mix/page.tsx
+++ ust_dig/src/app/selected_mix/page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import './page.css';
+import VisualCard from "@/components/visual_card/visual_card";
+import MixPanel from "@/components/mix_panel/mix_panel";
 
 export default function Selected_Mix() {
     return (
         <main>
-            <div>test</div>
+            <div id="artistInfoWrapper">
+                <VisualCard></VisualCard>
+                <div id="artistInfo">
+                    <p>Artist Info:</p>
 
-        </main>
+                </div>
+            </div>
+
+            <div id="mixesWrapper">
+                <div><p>Mixes</p></div>
+                <MixPanel></MixPanel>
+            </div>
+        </main >
     )
 }
